a19d4d53056d2e41b6e563a142d3d2b98d81a07a
Daniel Thoma <dt@clickbar.dev>
2022-11-29 13:24:30 +0100

    fix config indentation

--- web/src/angular/tailwind.config.js
+++ web/src/angular/tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{html,ts}',
+    "./src/**/*.{html,ts}",
   ],
   theme: {
     extend: {
-        colors: {
-            'secondary': '#f5f5f5',
-        },
+      colors: {
+        secondary: "#f5f5f5",
+      },
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
+    require("@tailwindcss/forms"),
   ],
 }
