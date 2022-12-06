e20f0aabe85ff97c94fc91e4cce82447ec348c95
Daniel Thoma <dt@clickbar.dev>
2022-12-06 19:28:22 +0100

    add configuration file

--- infrastructure/src/jvmMain/kotlin/replace/ApplicationModule.kt
+++ infrastructure/src/jvmMain/kotlin/replace/ApplicationModule.kt
@@ -1,13 +1,16 @@
 package replace
 
+import com.typesafe.config.ConfigFactory
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
+import io.ktor.server.config.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.plugins.cors.routing.*
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.Json.Default.serializersModule
 import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.modules.contextual
+import org.litote.kmongo.coroutine.CoroutineDatabase
 import org.litote.kmongo.coroutine.coroutine
 import org.litote.kmongo.reactivestreams.KMongo
 import replace.datastore.MongoRepository
@@ -33,9 +36,22 @@ fun Application.applicationModule() {
         folderPath = "static"
         ignoreIfContains = Regex("^/api.*$")
     }
-    val client = KMongo.createClient().coroutine
-    val db = client.getDatabase("replace")
+
+    val config = HoconApplicationConfig(ConfigFactory.load())
+    val db = getDB(config)
+
     configureRouting(
         bookingRepository = MongoRepository(db.getCollection()),
     )
+}
+
+fun getDB(config: HoconApplicationConfig): CoroutineDatabase {
+    val host = config.tryGetString("ktor.database.host") ?: "localhost"
+    val port = config.tryGetString("ktor.database.port") ?: "27017"
+    val user = config.tryGetString("ktor.database.user") ?: "admin"
+    val password = config.tryGetString("ktor.database.password") ?: "password"
+    val database = config.tryGetString("ktor.database.database") ?: "replace-app"
+
+    val client = KMongo.createClient("mongodb://$user:$password@$host:$port").coroutine
+    return client.getDatabase(database)
 }
