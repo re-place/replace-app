d9786b50f3c283bd44dab4689625008212bdd780
mlo <jmlozano98@gmail.com>
2022-12-10 13:25:20 +0100

    Created CreateBookableEntityUseCase

--- application/src/jvmMain/kotlin/replace/usecase/bookableentity/CreateBookableEntityUseCase.kt
+++ application/src/jvmMain/kotlin/replace/usecase/bookableentity/CreateBookableEntityUseCase.kt
@@ -1,17 +1,30 @@
 package replace.usecase.bookableentity
 
 import org.bson.types.ObjectId
+import replace.datastore.BookableEntityRepository
 import replace.datastore.Repository
+import replace.dto.BookableEntityDto
 import replace.dto.BookingDto
 import replace.dto.toDto
 import replace.model.BookableEntity
+import replace.model.BookableEntityType
 import replace.model.Booking
 
 object CreateBookableEntityUseCase {
     suspend fun execute(
-        bookableEntityDto: BookableEntity,
-        bookingRepository: Repository<Booking>,
-    ): BookingDto {
-        TODO("Check if parent exists etc")
+        bookableEntityDto: BookableEntityDto,
+        bookableEntityRepository: Repository<BookableEntity>,
+        bookableEntityTypeRepository: Repository<BookableEntityType>,
+    ): BookableEntityDto {
+        val name = bookableEntityDto.name
+        val parentId = ObjectId(bookableEntityDto.id)
+        val type = bookableEntityTypeRepository.findOneById(ObjectId(bookableEntityDto.typeId))
+        checkNotNull(type) { "BookableEntityType ID not found" }
+
+        val bookableEntity = BookableEntity(name, type, parentId)
+
+        val insertedBookableEntity = bookableEntityRepository.insertOne(bookableEntity)
+        checkNotNull(insertedBookableEntity) { "Could not insert BookableEntity" }
+        return insertedBookableEntity.toDto()
     }
 }
