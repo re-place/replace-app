a918d08ef93e74b21cd135de99fff84e09ab2fc9
mlo <jmlozano98@gmail.com>
2022-12-10 14:37:43 +0100

    Fixed formatting for CreateBOokableEntityUseCase

--- application/src/jvmMain/kotlin/replace/usecase/bookableentity/CreateBookableEntityUseCase.kt
+++ application/src/jvmMain/kotlin/replace/usecase/bookableentity/CreateBookableEntityUseCase.kt
@@ -1,14 +1,11 @@
 package replace.usecase.bookableentity
 
 import org.bson.types.ObjectId
-import replace.datastore.BookableEntityRepository
 import replace.datastore.Repository
 import replace.dto.BookableEntityDto
-import replace.dto.BookingDto
 import replace.dto.toDto
 import replace.model.BookableEntity
 import replace.model.BookableEntityType
-import replace.model.Booking
 
 object CreateBookableEntityUseCase {
     suspend fun execute(
