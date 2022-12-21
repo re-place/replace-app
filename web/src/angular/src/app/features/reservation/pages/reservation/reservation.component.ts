681ddbeff8b4fda657339353495be399cdf3998a
Jose Miguel Lozano <jmlozano98@gmail.com>
2022-12-22 00:11:28 +0100

    User can now select Site, Floor and Bookable Entity to make the reservation

--- web/src/angular/src/app/features/reservation/pages/reservation/reservation.component.ts
+++ web/src/angular/src/app/features/reservation/pages/reservation/reservation.component.ts
@@ -1,10 +1,83 @@
-import { Component } from "@angular/core"
+import { Component, OnInit } from "@angular/core"
+import { MatSnackBar } from "@angular/material/snack-bar"
+
+import { BookableEntityDto, DefaultService, FloorDto, SiteDto } from "src/app/core/openapi"
 
 @Component({
     selector: "reservation",
     templateUrl: "./reservation.component.html",
     styles: [],
 })
-export class ReservationComponent {
+export class ReservationComponent implements OnInit {
+    sites: SiteDto[] = []
+    selectedSite?: SiteDto
+
+    floors: FloorDto[] = []
+    selectedFloor?: FloorDto
+
+    bookableEntities: BookableEntityDto[] = []
+    selectedEntity?: BookableEntityDto
+
+    constructor(
+        private readonly apiService: DefaultService,
+        private readonly snackBar: MatSnackBar,
+    ) { }
+
+    ngOnInit() {
+        this.apiService.apiSiteGet().subscribe({
+            next: response => {
+                this.sites = response
+                this.alphaSort(this.sites)
+            },
+            error: () => {
+                this.showErrorSnackbar("Standort konnten nicht abgefragt werden")
+            },
+        })
+    }
+
+    // TODO:
+    // If floors.length == 1
+    //  pick the only element
+    //  mark corresponding field as read only
+    //  be sure to call getBookEnts manually
+    getFloors() {
+        if(this.selectedSite?.id == undefined) return
+
+        this.apiService.apiSiteSiteIdFloorGet(this.selectedSite.id).subscribe({
+            next: response => {
+                this.floors = response
+                this.alphaSort(this.floors)
+            },
+            error: () => {
+                this.showErrorSnackbar("Stockwerke für den Standort konnten nicht abgefragt werden")
+            },
+        })
+
+    } 
+
+    getBookableEntities() {
+        if(this.selectedFloor?.id == undefined) return
+
+        this.apiService.apiFloorFloorIdBookableEntityGet(this.selectedFloor.id).subscribe({
+            next: response => {
+                this.bookableEntities = response
+                this.alphaSort(this.bookableEntities)
+            },
+            error: () => {
+                this.showErrorSnackbar("Buchbare Objekte konnten nicht abgefragt werden")
+            },
+        })
+    }
+
+    alphaSort(arr: any[]) {
+        arr.sort((a,b) => {
+            if(a?.name == undefined || b?.name == undefined) return -1
+            return a.name > b.name ? 1 : -1
+        })
+    }
+
+    showErrorSnackbar(message: string) {
+        this.snackBar.open(message, "error", { duration: 3000 })
+    }
 
 }
--- web/src/angular/src/app/features/reservation/reservation.module.ts
+++ web/src/angular/src/app/features/reservation/reservation.module.ts
@@ -1,5 +1,10 @@
 import { CommonModule } from "@angular/common"
 import { NgModule } from "@angular/core"
+import { FormsModule } from "@angular/forms"
+import { MatButtonModule } from "@angular/material/button"
+import { MatFormFieldModule } from "@angular/material/form-field"
+import { MatInputModule } from "@angular/material/input"
+import { MatSelectModule } from "@angular/material/select"
 
 import { ReservationComponent } from "./pages/reservation/reservation.component"
 import { ReservationRoutingModule } from "./reservation-routing.module"
@@ -12,6 +17,11 @@ import { ReservationRoutingModule } from "./reservation-routing.module"
     imports: [
         CommonModule,
         ReservationRoutingModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatInputModule,
+        FormsModule,
+        MatButtonModule,
     ],
 })
 export class ReservationModule { }
