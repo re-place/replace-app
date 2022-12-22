46720edb174856daf27bc97d030f160a19526ebb
Daniel Thoma <dt@clickbar.dev>
2022-12-22 23:23:04 +0100

    switch to session cookie, so we can load images

--- web/src/angular/src/app/core/core.module.ts
+++ web/src/angular/src/app/core/core.module.ts
@@ -1,21 +1,13 @@
 import { CommonModule } from "@angular/common"
-import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http"
+import { HttpClientModule } from "@angular/common/http"
 import { NgModule, Optional, SkipSelf } from "@angular/core"
 
 import { AuthGuard } from "./guards/auth.guard"
-import { SessionInterceptor } from "./interceptors/session.interceptor"
 
 @NgModule({
     declarations: [],
     imports: [CommonModule, HttpClientModule],
-    providers: [
-        {
-            provide: HTTP_INTERCEPTORS,
-            useClass: SessionInterceptor,
-            multi: true,
-        },
-        AuthGuard,
-    ],
+    providers: [AuthGuard],
 })
 export class CoreModule {
     constructor(@Optional() @SkipSelf() coreModule: CoreModule | null) {
