483f8834c31dac5c01c6b227e23d7b5c69edc249
Daniel Thoma <dt@clickbar.dev>
2023-01-17 17:53:00 +0100

    add liquibase

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -1,7 +1,11 @@
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 @Suppress("DSL_SCOPE_VIOLATION") // https://youtrack.jetbrains.com/issue/KTIJ-19369
 plugins {
     id("kotlin-jvm.base-conventions")
     kotlin("plugin.serialization")
+    alias(libs.plugins.liquibase)
 }
 
 dependencies {
@@ -20,10 +24,58 @@ dependencies {
     jvmMainImplementation(libs.tegral.openapi.base)
     jvmMainImplementation(libs.tegral.openapi.swagger)
     jvmTestImplementation(libs.kotest)
+    liquibaseRuntime("org.liquibase:liquibase-core:4.18.0")
+    liquibaseRuntime("info.picocli:picocli:4.7.0")
+    liquibaseRuntime("org.liquibase:liquibase-groovy-dsl:3.0.2")
+    liquibaseRuntime("org.postgresql:postgresql:42.5.1")
 }
+
+apply(plugin = "liquibase")
 
 tasks {
     test {
         useJUnitPlatform()
+    }
+}
+
+val migrationDir = File("infrastructure/src/jvmMain/resources/db/migrations")
+val migrationRoot = File("infrastructure/src/jvmMain/resources/db/changelog-root.json")
+
+liquibase {
+    activities {
+        register("main") {
+            val dbUrl = "jdbc:postgresql://localhost:5432/replace-app" // by project.extra.properties
+            val dbUser = "postgres" // by project.extra.properties
+            val dbPass = "postgres" // by project.extra.properties
+
+            this.arguments = mapOf(
+                "logLevel" to "info",
+                "changeLogFile" to migrationRoot.path,
+                "url" to dbUrl,
+                "username" to dbUser,
+                "password" to dbPass
+            )
+        }
+    }
+
+    runList = "main"
+}
+
+val migrationStub = File("infrastructure/src/jvmMain/resources/db/changelog-stub.json")
+
+tasks {
+    register("make-migration") {
+        val migrationName = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss"))
+        val migrationFile = File("${migrationDir.path}/$migrationName.json")
+
+        migrationStub.copyTo(migrationFile)
+
+        doLast {
+            println("Created migration file: ${migrationFile.path}")
+
+            migrationStub.readText().replace("{{id}}", migrationName).also {
+                migrationFile.writeText(it)
+            }
+        }
     }
 }
