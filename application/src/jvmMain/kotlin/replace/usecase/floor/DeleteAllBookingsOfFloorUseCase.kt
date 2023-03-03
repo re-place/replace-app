35e94c200ef590c9b5b3c782ce4e45320be062bc
Arpon <mmorenoarp@gmail.com>
2023-03-03 13:10:25 +0100

    DeleteBookings added

--- application/src/jvmMain/kotlin/replace/usecase/floor/DeleteAllBookingsOfFloorUseCase.kt
+++ application/src/jvmMain/kotlin/replace/usecase/floor/DeleteAllBookingsOfFloorUseCase.kt
@@ -4,12 +4,14 @@ import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.deleteWhere
 import org.jetbrains.exposed.sql.transactions.transaction
 import replace.model.BookedEntities
+import replace.model.Bookings
 
 object DeleteAllBookingsOfFloorUseCase {
     suspend fun execute(
-        id: String
+        bookableEntityId: String
     ) {
         return transaction {
+            Bookings.deleteWhere { Bookings.id eq bookableEntityId }
             BookedEntities.deleteWhere { bookable_entity_id eq id }
         }
     }
