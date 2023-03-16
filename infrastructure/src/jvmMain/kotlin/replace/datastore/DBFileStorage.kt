46855f302939e8e4ae99b013a7d7a3e8b9a531a2
Alexander Städing <alexanderstaeding@gmail.com>
2023-03-16 19:37:20 +0100

    transaction -> newSuspendedTransaction in DBFileStorage

--- infrastructure/src/jvmMain/kotlin/replace/datastore/DBFileStorage.kt
+++ infrastructure/src/jvmMain/kotlin/replace/datastore/DBFileStorage.kt
@@ -2,60 +2,48 @@ package replace.datastore
 
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.deleteWhere
-import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import java.io.InputStream
 
 class DBFileStorage : FileStorage {
 
-    override suspend fun exists(path: String): Boolean {
-        return transaction {
-            return@transaction DBFile.findById(path) != null
-        }
+    override suspend fun exists(path: String): Boolean = newSuspendedTransaction {
+        DBFile.findById(path) != null
     }
 
-    override suspend fun saveFile(path: String, input: InputStream): Boolean {
-        return transaction {
-            DBFile.new(path) {
-                data = input.readBytes()
-            }
-
-            return@transaction true
+    override suspend fun saveFile(path: String, input: InputStream): Boolean = newSuspendedTransaction {
+        DBFile.new(path) {
+            data = input.readBytes()
         }
+
+        true
     }
 
-    override suspend fun deleteFile(path: String): Boolean {
-        return transaction {
-            DBFiles.deleteWhere { DBFiles.id eq path }
-
-            return@transaction true
-        }
+    override suspend fun deleteFile(path: String): Boolean = newSuspendedTransaction {
+        DBFiles.deleteWhere { id eq path }
+        true
     }
 
-    override suspend fun copyFile(from: String, to: String): Boolean {
-        return transaction {
-            val oldFile = DBFile.findById(from) ?: return@transaction false
+    override suspend fun copyFile(from: String, to: String): Boolean = newSuspendedTransaction {
+        val oldFile = DBFile.findById(from) ?: return@newSuspendedTransaction false
 
-            DBFile.new(to) {
-                data = oldFile.data
-            }
-
-            return@transaction true
+        DBFile.new(to) {
+            data = oldFile.data
         }
+
+        true
     }
 
     override suspend fun readFile(path: String): InputStream {
-        return transaction {
-            val file = DBFile.findById(path) ?: return@transaction InputStream.nullInputStream()
-
-            return@transaction file.data.inputStream()
+        return newSuspendedTransaction {
+            DBFile.findById(path)?.data?.inputStream()
+                ?: InputStream.nullInputStream()
         }
     }
 
-    override suspend fun getFileSize(path: String): Long {
-        return transaction {
-            val file = DBFile.findById(path) ?: return@transaction 0L
+    override suspend fun getFileSize(path: String): Long = newSuspendedTransaction {
+        val file = DBFile.findById(path) ?: return@newSuspendedTransaction 0L
 
-            return@transaction file.data.size.toLong()
-        }
+        file.data.size.toLong()
     }
 }
